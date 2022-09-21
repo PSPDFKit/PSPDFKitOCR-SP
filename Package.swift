60c1461a905fa0360be2230f4cfa9706406ff81e
Douglas Hill <hello@douglashill.co>
2022-09-21 15:59:28 +0200

    Update for PSPDFKit 11.5.1 for iOS

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PSPDFKitOCR",
-            url: "https://customers.pspdfkit.com/ocr/xcframework/11.5.0.zip",
-            checksum: "1ccf688a118fc1d640ea8645124fedc2dc33bdf947e005021f3b7375ee36315f"),
+            url: "https://customers.pspdfkit.com/ocr/xcframework/11.5.1.zip",
+            checksum: "1c0ea12c0dff6fbf6ec7019da7595e6c85827dadbda1135113e8b87b19e4ebc2"),
     ]
 )
