6f91c4ff45861dea7436dab8bbbcad2288c4e1f2
Stefan Kieleithner <stefan.kieleithner@gmail.com>
2024-01-24 08:41:30 +0100

    Add visionOS support

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ let package = Package(
     name: "PSPDFKitOCR",
     platforms: [
         .iOS(.v15),
-        .macCatalyst(.v15)
+        .macCatalyst(.v15),
+        .visionOS(.v1)
     ],
     products: [
         .library(
